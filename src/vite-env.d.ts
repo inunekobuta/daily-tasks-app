b73f3334f48cb38df56a67e63f5948ae71309306
inunekobuta <ken1206.quanta@gmail.com>
2025-08-13 17:22:52 +0900

    Fix: build (remove default React import, add index.css)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,1 @@
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://ndkaonebqwaqoempehxc.supabase.co";
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ka2FvbmVicXdhcW9lbXBlaHhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwNTgxNzksImV4cCI6MjA3MDYzNDE3OX0.F74K00d3zz4R5qsKhNt0AcRNHslGoxboG3el-c-CAIE";
-/// <reference types="vite/client" />
+declare module "*.css";
